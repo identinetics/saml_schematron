b19f0618e3b9bc48fd6eee6a63d89c59509d97c1
rhoerbe <rainer@hoerbe.at>
2014-02-21 13:26:48 +0100

    make executeable from outside project root

--- scripts/listrules.sh
+++ scripts/listrules.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xml-strings profiles/single_schematron_schemas/rule* :/ | grep -v '^\s*$' |sort -k 2
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJROOT="$(dirname "$SCRIPTDIR")"
+/opt/local/bin/xml-strings $PROJROOT/profiles/single_schematron_schemas/rule* :/ | grep -v '^\s*$' |sort -k 2
