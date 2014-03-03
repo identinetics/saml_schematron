523c19f5f00510714e248b9ab9e4ff58eccf3bb6
rhoerbe <rainer@hoerbe.at>
2014-03-03 19:32:44 +0100

    fixed bug with webapp validator

--- scripts/xsd_validation_xmlsectool.sh
+++ scripts/xsd_validation_xmlsectool.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # validate saml md schema. wrapper for xmlsectool
 
-XMLSECTOOL='/usr/local/xmlsectool-1.2.0/xmlsectool.sh'
-SAMLMETAXMLSCHEMAPATH='/usr/local/saml-schematron/xmlschema/'
+XMLSECTOOLDIR='xmlsectool-1.2.0'
+XMLSECTOOL="/usr/local/${XMLSECTOOLDIR}/xmlsectool.sh"
+SAMLMETAXMLSCHEMAPATH='/usr/local/saml_schematron/xmlschema/'
 if [ -z "$JAVA_HOME" ]; then
     export JAVA_HOME=/etc/alternatives/jre
 fi  
 
-echo "=== validate using xmlsectool"
-$XMLSECTOOL --validateSchema --schemaDirectory $SAMLMETAXMLSCHEMAPATH --inFile $0 2>&1
+echo "=== validate using ${XMLSECTOOLDIR}"
+$XMLSECTOOL --validateSchema --schemaDirectory $SAMLMETAXMLSCHEMAPATH --inFile $1  2>&1
 
 exit 0
