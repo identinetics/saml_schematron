330f6961ff4c6f4b5fd9141b77829a921bf9ba37
rhoerbe <rainer@hoerbe.at>
2014-05-15 17:42:19 +0200

    corrected reference to xsltproc

--- scripts/val_allrules_libxslt_long.sh
+++ scripts/val_allrules_libxslt_long.sh
@@ -7,5 +7,5 @@ PROJROOT="$(dirname "$SCRIPTDIR")"
 source $PROJROOT/scripts/init.sh
 
 file=`/bin/echo $1 | /usr/bin/sed -e 's/.xml$/.txt/'`
-${RUNXSLTPROC}xsltproc $PROJROOT/validate/allrules.xsl $1 >$PROJROOT/reports/$file 2>&1
+${RUNXSLTPROC} $PROJROOT/validate/allrules.xsl $1 >$PROJROOT/reports/$file 2>&1
 echo "output written to reports/$file"
