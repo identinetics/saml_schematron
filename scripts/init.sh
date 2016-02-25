1a0ac7b212581a1d65f03627264ef7bd94899582
r2h2 <rainer@hoerbe.at>
2016-02-25 20:03:00 +0100

    improve OS-dependent filesystem locations

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 # define system-dependend file locations
 
-export RUNXSLTPROC='/opt/local/bin/xsltproc'  # Macports
-#export RUNXSLTPROC='/usr/bin/xsltproc'  # Redhat
+if [[ "$ostype" == "linux-gnu" ]]; then
+    #  tested with CentOS7
+    export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
+    export RUNXSLTPROC='/usr/bin/xsltproc'
+    export RUNXMLLINT='/usr/bin/xmllint'
+    export RUNXMLCOREUTILS='/usr/bin/'
+elif [[ "$ostype" == "linux" ]]; then
+    #  tested with RHEL6
+    export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
+    export RUNXSLTPROC='/usr/bin/xsltproc'
+    export RUNXMLLINT='/usr/bin/xmllint'
+    export RUNXMLCOREUTILS='/usr/bin/'
+elif [[ "$ostype" == "darwin" ]]; then   #  used for OSX development env
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
+    export RUNXSLTPROC='/opt/local/bin/xsltproc'  # Macports
+    export RUNXMLLINT='/opt/local/bin/xmllint'  # Macports
+    export RUNXMLCOREUTILS='/opt/local/bin/' # Macports
+else
+    echo "no environment defined for $ostype"
+    exit 1
+fi
 
-export RUNXMLLINT='/opt/local/bin/xmllint'  # Macports
-#export RUNXMLLINT='/usr/bin/xmllint'  # Redhat
-
-export RUNXMLCOREUTILS='/opt/local/bin/' # Macports
-#export RUNXMLCOREUTILS='/usr/bin/' # Redhat
