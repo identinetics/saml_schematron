5b90c16473916da81ea7b06cf1453d33008f1c96
r2h2 <rainer@hoerbe.at>
2016-03-04 20:00:43 +0100

    static IP; scripts consistent with template

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIR=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 source $SCRIPTDIR/conf.sh
 
 if [ $(id -u) -ne 0 ]; then
     sudo="sudo"
 fi
-${sudo} docker rmi -f $IMAGENAME
-${sudo} docker build
+${sudo} docker rmi -f $IMAGENAME 2> /dev/null || true
+${sudo} docker build \
     --build-arg "USERNAME=$CONTAINERUSER" \
     --build-arg "UID=$CONTAINERUID" \
     -t=$IMAGENAME .
--- docker/conf.sh
+++ docker/conf.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
-export CONTAINERNAME='mdvalTestPvAt'
-export HOSTVOLROOT="/docker_volumes/$CONTAINERNAME"  # container volumes on docker host
-export IMAGENAME='saml_schematron'
-export INTERCONTAINER_NETWORK='http_proxy'
-export WEBAPPURL='http://mdval.test.portalverbund.at/'
-export CONTAINERUSER='samlschtron'   # group and user to run container
-export CONTAINERUID='8000'   # gid and uid for CONTAINERUSER
+export IMGID='4'  # range from 2 .. 99; must be unique
+export IMAGENAME="r2h2/samlschtron${IMGID}"
+export IMGID='4'  # range from 1 .. 99; must be unique
+export CONTAINERNAME="${IMGID}mdvalTestPvAt"
+export CONTAINERUID="800${IMGID}"   # gid and uid for CONTAINERUSER
+export CONTAINERUSER="samlschtron${IMGID}"   # group and user to run container
+export ENVSETTING=""
+export NETWORKSETTINGS="
+    --net http_proxy
+    --ip 10.1.1.${IMGID}
+"
+export STARTCMD='/start.sh'
+export VOLROOT="/docker_volumes/$CONTAINERNAME"  # container volumes on docker host
+export VOLMAPPING="-v $VOLROOT/var/log:/var/log:Z"
+
+# first start: create user/group/host directories
+if ! id -u $CONTAINERUSER &>/dev/null; then
+    groupadd -g $CONTAINERUID $CONTAINERUSER
+    adduser -M -g $CONTAINERUID -u $CONTAINERUID $CONTAINERUSER
+fi
+if [ -d $VOLROOT/var/log/$CONTAINERNAME ]; then
+    mkdir -p $VOLROOT/var/log
+    chown $CONTAINERUSER:$CONTAINERUSER $VOLROOT/var/log
+fi
+
+# create dir with given user if not existing, relative to $HOSTVOLROOT
+function chkdir {
+    dir=$1
+    user=$2
+    mkdir -p "$VOLROOT/$dir"
+    chown -R $user:$user "$VOLROOT/$dir"
+}
+chkdir var/log $CONTAINERUSER
