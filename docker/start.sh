bc1d16cb7ab3b207b564b0e4ce79707868e2273c
r2h2 <rainer@hoerbe.at>
2016-04-20 23:03:00 +0200

    fixed

--- docker/start.sh
+++ docker/start.sh
@@ -3,4 +3,4 @@
 # validate_srv.py        Startup script for the saml schematron validation web app
 
 cd /opt/saml_schematron/webapp/
-python3.4 ./validate_srv.py > /var/log/access.log 2> /var/log/error.log
+validate_srv.py > /var/log/access.log 2> /var/log/error.log
