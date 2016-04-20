df5f340c3c524fbc74210c0ad84b754cf61fccee
r2h2 <rainer@hoerbe.at>
2016-04-20 23:39:15 +0200

    fixed template files

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# validate_srv.py        Startup script for the saml schematron validation web app
+# Startup script for the saml schematron validation web app
 
 cd /opt/saml_schematron/webapp/
-validate_srv.py > /var/log/access.log 2> /var/log/error.log
+start_srv.py > /var/log/access.log 2> /var/log/error.log
