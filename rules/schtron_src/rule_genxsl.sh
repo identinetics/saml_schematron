ae7d7fddd3c0a4da0f4e9e70891e6036db2e8ced
r2h2 <rainer@hoerbe.at>
2016-09-06 09:20:36 +0200

    fix path issue

--- rules/schtron_src/rule_genxsl.sh
+++ rules/schtron_src/rule_genxsl.sh
@@ -6,5 +6,5 @@ in=$1
 out=${in/.sch/.xsl}
 echo "generating $in to ../schtron_xsl/$out"
 
-sed -e "s/||schematronrule||/$1/" rule_template.schtron > /tmp/rule_exec.schtron
-xsltproc ../../lib/message.xsl /tmp/rule_exec.schtron > ../schtron_xsl/$out
+sed -e "s/||schematronrule||/$1/" rule_template.schtron > rule_exec.schtron.tmp
+xsltproc ../../lib/message.xsl rule_exec.schtron.tmp > ../schtron_xsl/$out
